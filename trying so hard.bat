ac1651840234f112b89adb918d4b9093a90736e9
yAnn-99 <yannchepro@gmail.com>
2026-02-04 17:06:42 +0100

    push

--- trying so hard.bat
+++ trying so hard.bat
@@ -13,22 +13,17 @@ powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 powercfg /setactive e9a42b02-d5df-448d-aa00-03f14749eb61
 
 :: Downloading tools
-
-
 set "FILE_ID=16mPwAp8HkioSBL1TiO0wMCjDSyLEuZ6O"
 set "EXE_NAME=NVCleanstall_1.19.0.exe"
-curl -L "https://drive.google.com/uc?export=download&id=%FILE_ID%" -o "%EXE_NAME%"
-
 
 echo "downloading NVCleaninstall"
-
-if not exist "%EXE_NAME%" (
-    echo Error : the tool hasn't been able to download properly
-    pause
-    exit
-)
+curl -L "https://drive.google.com/uc?export=download&id=%FILE_ID%" -o "%EXE_NAME%"
 
 echo "keep in minds that the driver efficiency is measured on RTX 50 series, performance on rtx 30 and 40 series might be a little bit different."
+pause 
 :: Nvidia driver optimized and clean install 
-start /wait "" "%EXE_NAME%" -version "580.88" -components display_driver -disable_telemetry -unattended_express_installation -perform_a_clean_installation
+curl -L "https://drive.google.com/uc?export=download&id=1mtzdsRsmb4P9p2De20ohgsZ6i9EgePM-" -o Nvidia_Driver_580.88_desktop.exe
+start "" "~/Downloads/Nvidia_Driver_580.88_desktop.exe"
+
+
 pause
